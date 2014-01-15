c86bd9bf03e80d37b71ce4c4c791d00e1cec683a
aschulhofer <andreas.schulhofer@gmail.com>
2014-01-15 11:10:05 +0100

    begin browse/list, present

--- index.py
+++ index.py
@@ -12,8 +12,45 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from datetime import datetime
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1445@localhost/knuth_db';
+
+db = SQLAlchemy(app);
+
+class Document(db.Model):
+    __tablename__ = "documents"
+    id = db.Column(db.Integer, primary_key = True)
+    type = db.Column(db.String(20))
+    title = db.Column(db.String(256))
+    author = db.Column(db.String(40))
+    timestamp = db.Column(db.Integer)
+    parent = db.Column(db.Integer)
+
+    def __init__(self):
+        print 'Document:' % (self.id)
+        
+    def __repr__(self):
+        return self.title
+
+    def getDate(self):
+        return datetime.utcfromtimestamp(self.timestamp)
+
+class Metadata(db.Model):
+    __tablename__ = "metadata"
+    id = db.Column(db.Integer, primary_key = True)
+    document = db.Column(db.Integer)
+    key = db.Column(db.String(40))
+    value = db.Column(db.Text)
+
+    def __init__(self):
+        print 'Metadata:' % (self.id)
+        
+    def __repr__(self):
+        return self.key
+
 
 
 @app.route('/faq')
@@ -40,8 +77,14 @@ def create(methods=['GET', 'POST']):
 @app.route('/list/<int:page>')
 def list(page=0, methods=['GET']):
     # 1. request data from DB
+    documents = Document.query.all()
+    
+    document_dict = {}
+    for doc in documents:
+        document_dict[str(doc.id)] = doc
+        
     # 2. provide data to template engine
-    return render_template('list.html', **{'documents' : []})
+    return render_template('list.html', **{'documents' : document_dict})
 
 
 @app.route('/update')
@@ -50,13 +93,16 @@ def update(methods=['POST', 'PUT']):
 
 
 @app.route('/delete/<int:doc_id>')
-def delete(doc_id):
+def delete(doc_id, methods=['DELETE']):
     return render_template('doc.html', doc_id=doc_id)
 
 
 @app.route('/doc/<int:doc_id>')
-def doc(doc_id, methods=['DELETE']):
-    return render_template('doc.html')
+def doc(doc_id):
+    document = Document.query.filter_by(id=doc_id).first()
+    metadata = Metadata.query.filter_by(document=doc_id).all()
+    
+    return render_template('doc.html', **{'document' : document, 'metadata' : metadata})
 
 
 @app.route('/')
